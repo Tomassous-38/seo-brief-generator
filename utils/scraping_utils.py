f35c16b41404c6e7589203d6ccc2a8ea48675f7a
Tomassous-38 <57865102+Tomassous-38@users.noreply.github.com>
2024-06-16 21:27:07 +0200

    Create scraping_utils.py

---
+++ utils/scraping_utils.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_text_from_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    paragraphs = soup.find_all('p')
+    text = ' '.join([para.get_text() for para in paragraphs])
+    return text
