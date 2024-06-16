8730d65e5d188781f9c31dbfa8af71279bc9f64c
Tomassous-38 <57865102+Tomassous-38@users.noreply.github.com>
2024-06-16 21:36:54 +0200

    Update serpapi_utils.py

--- utils/serpapi_utils.py
+++ utils/serpapi_utils.py
@@ -1,10 +1,12 @@
-from serpapi import GoogleSearch
+from serpapi.google_search_results import GoogleSearch
 
 def get_google_search_results(query, api_key, num_results=3):
-    search = GoogleSearch({
+    params = {
         "q": query,
-        "api_key": api_key
-    })
+        "api_key": api_key,
+        "num": num_results
+    }
+    search = GoogleSearch(params)
     results = search.get_dict()
     urls = []
     for result in results.get("organic_results", [])[:num_results]:
